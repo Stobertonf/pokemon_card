6e015e492457ce2219811e9e8a5fa2a813be0833
stobertonf <stobertonf@gmail.com>
2023-07-31 04:00:29 -0300

    feat(Packages): Creating the Home Page file.

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'home_store.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
