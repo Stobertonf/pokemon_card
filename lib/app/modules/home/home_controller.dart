a85557214d736061a721a9582a68660e90d08dfd
stobertonf <stobertonf@gmail.com>
2023-08-01 03:31:09 -0300

    feat(Home-Page): Adding Package.

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,14 +1,17 @@
 import 'package:mobx/mobx.dart';
+import 'package:injectable/injectable.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pokemon_card/app/modules/home/home_module.dart';
 import 'package:pokemon_card/app/modules/home/domain/pokemon.dart';
 import 'package:pokemon_card/app/modules/home/pokemon_repository.dart';
 
 part 'home_controller.g.dart';
 
+@Injectable()
 class HomeController = _HomeControllerBase with _$HomeController;
 
 abstract class _HomeControllerBase with Store {
-  final PokemonRepository repository = Modular.get<PokemonRepository>();
+  final PokemonRepository repository = HomeModule.to.get<PokemonRepository>();
 
   @observable
   List<Pokemon> pokemons = ObservableList();
