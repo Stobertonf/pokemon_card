4cb14e875c7f2561684e4c15978aed65a680086a
stobertonf <stobertonf@gmail.com>
2023-08-01 02:40:54 -0300

    feat(Details-Page): Creating the Home Controller file.

---
+++ lib/app/modules/home/home_controller.dart
@@ -0,0 +1,35 @@
+import 'package:mobx/mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pokemon_card/app/modules/home/domain/pokemon.dart';
+import 'package:pokemon_card/app/modules/home/pokemon_repository.dart';
+
+part 'home_controller.g.dart';
+
+class HomeController = _HomeControllerBase with _$HomeController;
+
+abstract class _HomeControllerBase with Store {
+  final PokemonRepository repository = Modular.get<PokemonRepository>();
+
+  @observable
+  List<Pokemon> pokemons = ObservableList();
+
+  @action
+  atualizarPokemonsObtidos() async {
+    pokemons = await _atualizarPokemonsObtidos();
+  }
+
+  List<Pokemon> getPokemonsObtidos() => pokemons;
+
+  Future<List<Pokemon>> _atualizarPokemonsObtidos() {
+    return repository.getPokemonsObtidos();
+  }
+
+  Future<List<Pokemon>> getAllPokemons() {
+    return repository.getAllPokemons();
+  }
+
+  adicionarListaObtidos(Pokemon pokemon) {
+    repository.adicionarListaObtidos(pokemon);
+    atualizarPokemonsObtidos();
+  }
+}
